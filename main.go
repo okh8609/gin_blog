fa899e9b372c666a96cd93cf86cabd9e65ca3081
Khaos <okh8609@gmail.com>
2021-04-21 17:16:38 +0800

    gin init
    .

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.HandleMethodNotAllowed = true
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"msg": "pong"})
+	})
+	r.Run()
+}
